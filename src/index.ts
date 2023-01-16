818ce9b11102a36af776d08675b1663f2fb82c90
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2023-01-17 01:39:39 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,38 @@ const handleRequest = async (
 
   reflare.push({
     path: '/*',
-    upstream: {
-      domain: 'httpbin.org',
-      protocol: 'https',
+    loadBalancing: {
+      policy: 'random',
+    },
+    upstream: [
+      {
+        domain: 'https://gdindex.phantomdroida.workers.dev',
+        protocol: 'https',
+      },
+      /*{
+        domain: 'https://gdindex.phantomdroidb.workers.dev',
+        protocol: 'https',
+      },*/
+      {
+        domain: 'https://gdindex.phantomdroidc.workers.dev',
+        protocol: 'https',
+      },
+      {
+        domain: 'https://gdindex.phantomdroidx.workers.dev',
+        protocol: 'https',
+      },
+      {
+        domain: 'https://gdindex.phantomdroidy.workers.dev',
+        protocol: 'https',
+      },
+      {
+        domain: 'https://gdindex.phantomdroidz.workers.dev',
+        protocol: 'https',
+      },
+    ]
+    cors: {
+      origin: '*',
+      credentials: true,
     },
   });
 
